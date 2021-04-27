622daef02cd49fe1e2b53ff2a1785daec1f11c3d
sam <sam.suoan@gmail.com>
2021-04-27 19:17:33 +0800

    todo完成

--- cli.js
+++ cli.js
@@ -1,4 +1,8 @@
+#!/usr/bin/env node
 const program = require('commander');
+const api = require('./index.js')
+
+
 program
   .option('-x, --xxx', 'what the x')
 
@@ -7,7 +11,17 @@ program
   .description('add a task')
   .action((...args) => {
     const words = args.slice(0, args.length - 1).join(' ')
-    console.log(words);
+    api.add(words).then(() => {console.log('添加成功')}, () => {console.log('添加失败')})
+  });
+program
+  .command('clear')
+  .description('clear all task')
+  .action(() => {
+    api.clear().then(() => {console.log('清除成功')}, () => {console.log('清除失败')});
   });
 
 program.parse(process.argv);
+
+if (process.argv.length === 2) {
+  void api.showAll()
+}
